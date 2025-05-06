2ad86b067185d883b5c48d7e9fccacf985ed1a73
nathanolszowski <olszowski.nathan@outlook.fr>
2025-05-06 16:57:25 +0200

    Update settings
    Update settings suite à amélioration module user agent

--- config/settings.py
+++ config/settings.py
@@ -2,13 +2,15 @@
 """
 Configuration globale pour les scrapers
 """
+from utils.user_agent import Rotator
+from utils.liste_user_agent import liste_user_agent
 
 # Départements d'Île-de-France ciblés
 DEPARTMENTS_IDF = ["75", "77", "78", "91", "92", "93", "94", "95"]
 
 # Timeouts et délais
 REQUEST_TIMEOUT = 5  # secondes
-SELENIUM_WAIT_TIME = 4  # secondes
+SELENIUM_WAIT_TIME = 5  # secondes
 
 # Configuration Selenium
 SELENIUM_OPTIONS = [
@@ -21,7 +23,9 @@ SELENIUM_OPTIONS = [
     "window-size=1920x1080"
 ]
 
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
+USER_AGENT = Rotator(liste_user_agent())
+
 
 # URLs des sitemaps
 SITEMAPS = {
