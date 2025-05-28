8ae3d1d4940ff97d22b23a997898cad510171899
nathanolszowski <olszowski.nathan@outlook.fr>
2025-05-28 15:46:08 +0200

    MAJ (bnp) docstrings + noms variables

--- scrapers/bnp.py
+++ scrapers/bnp.py
@@ -19,8 +19,20 @@ class BNPScraper(RequestsScraper):
         super().__init__(ua_generateur, proxy, "BNP", SITEMAPS["BNP"])
         self.selectors = BNP_SELECTORS
 
-    def post_taitement_hook(self, data: dict, soup: BeautifulSoup, url: str) -> dict:
-        # Détermine le type de contrat
+    def post_taitement_hook(
+        self, data: dict, soup: BeautifulSoup, url: str
+    ) -> dict[str]:
+        """Méthode de post-traitement surchargée pour les besoins du scraper de BNP
+
+        Args:
+            data (dict[str]): Représente les données de l'offre à scraper
+            soup (BeautifulSoup): Représente le parser lié à la page html de l'offre à scraper
+            url (str): Représente l'url de l'offre à scraper
+
+        Returns:
+            dict[str]: Représente les données de l'offre à scraper après modification spécifique pour un scraper
+        """
+        # Surcharger la méthode obtenir contrat
         if "a-louer" in url:
             data["contrat"] = "Location"
             data["prix_global"] = self.safe_select_text(
@@ -33,7 +45,7 @@ class BNPScraper(RequestsScraper):
             )
         else:
             data["contrat"] = "N/A"
-        # Déterminer le type d'actif
+        # Surcharger la méthode obtenir l'actif
         actif_map = {
             "bureau": "Bureaux",
             "local": "Locaux d'activité",
@@ -42,32 +54,33 @@ class BNPScraper(RequestsScraper):
         data["actif"] = next(
             (label for key, label in actif_map.items() if key in url), "N/A"
         )
-        # Déterminer l'adresse complète
+        # Surcharger la méthode obtenir l'adresse
         adresse = self.safe_select_text(soup, self.selectors["adresse"])
         nom_immeuble = self.safe_select_text(soup, self.selectors["nom_immeuble"])
         data["adresse"] = f"{nom_immeuble} {adresse}".strip()
 
-    def filtre_idf_bureaux(self, urls: list[str]) -> list[str]:
+    def filtre_urls(self, urls: list[str]) -> list[str]:
         """
-        Filtre les URLs pour supprimer les bureaux hors IDF
+        Méthode de filtrage surchargée pour les besoins du scraper BNP
 
         Args:
             urls (list[str]): Liste de chaînes de caractères représentant les urls à scraper
+
         Returns:
-            filtered_urls (list[str]): Liste de chaînes de caractères représentant les urls à scraper après filtrage des urls bureaux régions
+            urls_filtrees (list[str]): Liste de chaînes de caractères représentant les urls à scraper après filtrage des urls bureaux régions
         """
         logger.info("Filtrage des offres")
-        filtered_urls = []
+        urls_filtrees = []
         for url in urls:
             if "bureau" in url:
-                # Check if it contains any department from DEPARTMENTS_IDF
+                # Regarde si contient les départements IDF
                 if not any(
                     f"-{departement}/" in url for departement in DEPARTMENTS_IDF
                 ):
-                    filtered_urls.append(url)
+                    urls_filtrees.append(url)
             else:
-                filtered_urls.append(url)
+                urls_filtrees.append(url)
         logger.info(
-            f"[{self.name.upper()}] Trouvé {len(filtered_urls)} URLs filtrées sans bureaux région"
+            f"[{self.name.upper()}] Trouvé {len(urls_filtrees)} URLs filtrées sans bureaux région"
         )
-        return filtered_urls
+        return urls_filtrees
